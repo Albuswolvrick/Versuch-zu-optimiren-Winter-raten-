7efd63d348144ce223a88095c36ae23efac5c402
pierre <pierre.maurice.hesse@gmail.com>
2025-12-15 13:09:08 +0100

    flakes have now a side movment sped and get a random side but do not get a update to the side at all

--- try_2/src/main.rs
+++ try_2/src/main.rs
@@ -19,7 +19,6 @@ struct Snowflake {
     y: f32,
     speed: f32,
     side_toside_speed: f32,
-    side_to_side_range:f32,
     size: f32,
 }
 
@@ -163,8 +162,7 @@ impl MyApp
                 y: rng.gen_range(-0.8..0.0),
                 speed: rng.gen_range(0.001..0.0025),
                 size: rng.gen_range(2.0..11.0),
-                side_to_side_range:rng.gen_range(2.0..11.0),
-                side_toside_speed:rng.gen_range(0.001..0.0025),
+                side_toside_speed:rng.gen_range(-0.0025..0.0025),
             })
             .collect();
 
@@ -305,7 +303,16 @@ impl eframe::App for MyApp {
                 flake.x = rand::thread_rng().gen_range(0.0..1.0);
             }
         }
-        // I do not know how to ad ad side to side movment please add when posible and push 
+        for flake in &mut self.snowflakes
+        {
+            flake.x += flake.side_toside_speed;
+            if flake.y > 1.1
+            {
+                flake.y = -0.1;
+                flake.x =rand::thread_rng().gen_range(0.0..10.)
+            }
+        }
+        // I do not know how to ad ad side to side movment please add when posible and push
 
         ctx.request_repaint();
 
